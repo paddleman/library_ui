8a6ffee687f3df84f39e35d8b1ce7c471575397e
DJ <rivers.creeks@gmail.com>
2019-03-10 12:42:42 -0700

    completed video 21 - create and save author

--- app/controllers/author/create.js
+++ app/controllers/author/create.js
@@ -2,16 +2,17 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 
-  reset() {
-    this.setProperties({
-      first: '',
-      last: ''
-    });
-  },
 
-  actions: {
+  actions: { 
+    saveAuthor(ev) {
+      ev.preventDefault();
 
+      let author = this.store.createRecord('author', this.model); 
+
+      author.save().then(() => {
+        this.transitionToRoute("author");
+      });
+    }
   }
-
 
 });
