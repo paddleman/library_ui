5591b712e2ad84864b9d04bdae31ca4a58786fb4
DJ <rivers.creeks@gmail.com>
2019-03-10 14:44:10 -0700

    completed video 25 - search functionality on ui

--- app/routes/author.js
+++ app/routes/author.js
@@ -2,8 +2,36 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 
-  model() {
+  queryParams: {
+    search: {
+      // refreshModel fires the model whenever the search parameter changes
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+    if (params.search) {
+      return this.store.query('author', {
+        filter: {
+          query: params.search
+        }
+      });
+    }
     return this.store.findAll('author');
   }
 
 });
+
+// same model hook with ES6 syntax:
+
+// model({ search }) {
+//   if (search) {
+//     return this.store.query('author', {
+//       filter: {
+//         query: search
+//       }
+//     });
+//   }
+//   return this.store.findAll('author');
+// }
+
