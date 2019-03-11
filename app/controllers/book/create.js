1daf69ed4d0d51f99df04128a30fa54ce490f60a
DJ <rivers.creeks@gmail.com>
2019-03-11 09:59:21 -0700

    completed video 32 - create book feature implemented

--- app/controllers/book/create.js
+++ app/controllers/book/create.js
@@ -3,8 +3,14 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 
   actions: {
-    selectAuthor(author) {
-      console.log(author)
+    saveBook(ev) {
+      ev.preventDefault();
+      let book = this.store.createRecord('book', this.model);
+
+      book.save().then(() => {
+        this.transitionToRoute('book');
+      });
+
     },
     searchAuthor(query) {
       return this.store.query('author', { filter: { query }});
--- app/routes/book/create.js
+++ app/routes/book/create.js
@@ -3,7 +3,14 @@ import Route from '@ember/routing/route';
 export default Route.extend({
 
   model() {
-    return this.store.findAll('author');
-  }
 
+
+    return {
+      title: '',
+      isbn: '',
+      // null is used so as not to disrupt ember power select
+      publishDate: null,
+      author: null
+    };
+  }
 });
