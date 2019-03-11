2b23e8664ed49a3706239294ce669889fc3fad60
DJ <rivers.creeks@gmail.com>
2019-03-10 22:32:39 -0700

    completed video 30 - power select filtering implemented

--- app/controllers/book/create.js
+++ app/controllers/book/create.js
@@ -5,6 +5,10 @@ export default Controller.extend({
   actions: {
     selectAuthor(author) {
       console.log(author)
+    },
+    searchAuthor(query) {
+      return this.store.query('author', { filter: { query }});
+      
     }
   }
 
