e1a1cceaaaf9873c17c8bfb4170a9f30c164fa87
DJ <rivers.creeks@gmail.com>
2019-03-17 10:34:58 -0700

    working thru liquid fire

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,7 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service'
+// import { inject as service } from '@ember/service'
 
 export default Route.extend({
 
-  moment: service('moment'),
-  
-  beforeModel() {
-    this.get('moment').setTimeZone('America/Vancouver');
-  }
   
 });
---
+++ app/transitions.js
@@ -0,0 +1,32 @@
+const duration = 300;
+
+export default function(){
+ 
+
+  this.transition(
+    this.hasClass('modal-if'),
+    this.use('fade', { duration })
+  ),
+
+  this.transition(
+    this.fromRoute('book.detail.index'),
+    this.use('toLeft', { duration }),
+    this.reverse('toRight', { duration })
+  ),
+
+  this.transition(
+    this.fromRoute('author.detail.index'),
+    this.use('toUp', { duration })
+  )
+
+
+
+
+  // Add your transitions here, like:
+  //   this.transition(
+  //     this.fromRoute('people.index'),
+  //     this.toRoute('people.detail'),
+  //     this.use('toLeft'),
+  //     this.reverse('toRight')
+  //   );
+}
